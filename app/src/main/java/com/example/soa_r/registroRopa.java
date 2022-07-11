eb3be2a7d82c2508dc02cf8152ad0baf69b89887
JACIM24 <61199377+JACIM24@users.noreply.github.com>
2022-07-11 10:19:00 -0500

    registro ropa

---
+++ app/src/main/java/com/example/soa_r/registroRopa.java
@@ -0,0 +1,14 @@
+package com.example.soa_r;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registroRopa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_ropa);
+    }
+}
