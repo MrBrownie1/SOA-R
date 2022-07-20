c2522bdd13b432149251a56705cf858227b96186
MrBrownie1 <emma.3a@micorreo.upp.edu.mx>
2022-07-20 18:13:10 -0500

    Creacion de vista datos

---
+++ app/src/main/java/com/example/soa_r/vistaDatos.java
@@ -0,0 +1,35 @@
+package com.example.soa_r;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.helper.widget.Carousel;
+
+import android.os.Bundle;
+
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class vistaDatos extends AppCompatActivity {
+
+    ImageCarousel carousel = findViewById(R.id.carousel);
+
+    List<CarouselItem> list = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_datos);
+
+        list.add(
+                new CarouselItem(
+                        "https://images.unsplash.com/photo-1534447677768-be436bb09401?w=1080"
+                )
+        );
+        carousel.addData(list);
+
+
+    }
+}
